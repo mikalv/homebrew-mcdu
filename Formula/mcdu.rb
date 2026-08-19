b7151c6be4e28c2c445e55ac07a26f9a67065f47
Mikal <m@meeh.dev>
2026-08-19 08:21:54 +0200

    mcdu 0.6.0

--- Formula/mcdu.rb
+++ Formula/mcdu.rb
@@ -1,26 +1,26 @@
 class Mcdu < Formula
   desc "Modern disk usage analyzer with TUI and developer cleanup tools"
   homepage "https://github.com/mikalv/mcdu"
-  version "0.5.0"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mikalv/mcdu/releases/download/v#{version}/mcdu-macos-aarch64.tar.gz"
-      sha256 "67b51ef435d94f2a9430d8aa5feadc21329604b2680829ce4a153f97ac2ce94e"
+      sha256 "adee3170492cc1c671984f81e5d8a286b0d24c5e704bf2509efa1b5b42cf683b"
     else
       url "https://github.com/mikalv/mcdu/releases/download/v#{version}/mcdu-macos-x86_64.tar.gz"
-      sha256 "60a8bf0f6ba77fcc9764cfe4aee8291bc2562bb7040c4d80a7a4d325279fbdf8"
+      sha256 "6fac459c4082d88db814b84d05d7a9ecd06f46749f104ecea6426af7930dddc4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/mikalv/mcdu/releases/download/v#{version}/mcdu-linux-aarch64-musl.tar.gz"
-      sha256 "9cb25918df98b0aa209c7e1ea76e8407c33d9cee521a654a164a1603b4b654f3"
+      sha256 "63bd973753185a24c425af60906c5fb68c6c6e0a7d67deab6d1cd627410a9977"
     else
       url "https://github.com/mikalv/mcdu/releases/download/v#{version}/mcdu-linux-x86_64-musl.tar.gz"
-      sha256 "e1a3d1b0569aa80bbb4e920f1e4cd66cb4eb20bc7124b749efdf93cddfd7975c"
+      sha256 "f3623d86a6fe13c02c15b2692d726be5e80360a890a0cac0e63047082d27421e"
     end
   end
 
