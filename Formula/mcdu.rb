5d5b257d833859a28621c7d4fc31dfa0dadfed4d
Mikal <m@meeh.dev>
2026-08-27 13:24:00 +0200

    mcdu 0.6.0: fix checksums for rebuilt release

--- Formula/mcdu.rb
+++ Formula/mcdu.rb
@@ -7,20 +7,20 @@ class Mcdu < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mikalv/mcdu/releases/download/v#{version}/mcdu-macos-aarch64.tar.gz"
-      sha256 "adee3170492cc1c671984f81e5d8a286b0d24c5e704bf2509efa1b5b42cf683b"
+      sha256 "acd6ce5231c56ed034892a8c7e083f5c9c69b9fb9fe2c2245c83d17649f2a11a"
     else
       url "https://github.com/mikalv/mcdu/releases/download/v#{version}/mcdu-macos-x86_64.tar.gz"
-      sha256 "6fac459c4082d88db814b84d05d7a9ecd06f46749f104ecea6426af7930dddc4"
+      sha256 "91b8f9335a518f8fd78893aa34c427a1e25b00bb14c84d4be8d7e74ef741b0cb"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/mikalv/mcdu/releases/download/v#{version}/mcdu-linux-aarch64-musl.tar.gz"
-      sha256 "63bd973753185a24c425af60906c5fb68c6c6e0a7d67deab6d1cd627410a9977"
+      sha256 "09a3868e721f69759bb8f013e87944a1c50d24a131208deb169682b82941c0b2"
     else
       url "https://github.com/mikalv/mcdu/releases/download/v#{version}/mcdu-linux-x86_64-musl.tar.gz"
-      sha256 "f3623d86a6fe13c02c15b2692d726be5e80360a890a0cac0e63047082d27421e"
+      sha256 "f97d4b7ad32681252c4f8cb62321e69f287579686bce36c3bcfa5e543c0e3597"
     end
   end
 
